11761bffda5dfef61183c19a58c2a3b68a89dc75
piyo56 <piyo56.net@gmail.com>
2016-12-08 00:26:24 +0900

    solved later（it took 1h 15m to solve T_T）

--- app/main.py
+++ app/main.py
@@ -1,86 +1,89 @@
-#!/usr/bin/env python3
+# coding: utf-8
 import sys
 
 def is_valid(argv):
+
+    # 引数の数をチェック
     if len(argv) < 3:
+        sys.stderr.write('引数の数が不正\n')
         return False
     
     hour   = argv[1]
     minute = argv[2]
-
-    if len(hour) > 2 or len(minute) > 2:
-        return False
     
+    # 数字が3桁以上
+    if len(hour) >= 3 or len(minute) >= 3:
+        sys.stderr.write('数字が3桁以上\n')
+        return False
+
+    # 数字の頭に0が入っている
     if hour[0] == "0" or minute[0] == "0":
+        sys.stderr.write('頭に0が入っている\n')
         return False
     
+    # 数字でないものが混ざっている
     valid_nums = [str(d) for d in range(1,10)]
-    for d in hour:
-        if not d in valid_nums:
+    for h in hour:
+        if not h in valid_nums:
+            sys.stderr.write('数字でないものがある\n')
             return False
-
-    for d in minute:
-        if not d in valid_nums:
+    for m in minute:
+        if not m in valid_nums:
+            sys.stderr.write('数字でないものがある\n')
             return False
 
     return True
 
 def calc_flags(num):
-   
-    flags = [0,0,0,0,0]
-    num = int(num)
-
-    if num == 11:
-        flags = [1,0,1,1,1]
-    if num == 10:
-        flags = [1,1,0,1,1]
-    if num == 9:
-        flags = [0,1,0,1,1]
-    if num == 8:
-        flags = [0,0,0,1,1]
-    if num == 7:
-        flags = [0,0,1,0,1]
-    if num == 6:
-        flags = [1,0,1,1,0]
-    if num == 5:
-        flags = [1,1,0,1,0]
-    if num == 4:
-        flags = [1,1,1,0,0]
-    if num == 3:
-        flags = [1,0,1,0,0]
-    if num == 2:
-        flags = [1,1,0,0,0]
     if num == 1:
-        flags = [1,0,0,0,0]
-    if num == 0:
-        flags = [0,0,0,0,0]
+        return [[1,0,0,0,0], [0,1,0,0,0]]
+    elif num == 2:
+        return [[1,1,0,0,0], [0,0,1,0,0]]
+    elif num == 3:
+        return [[1,0,1,0,0], [0,1,1,0,0], [0,0,0,1,0]]
+    elif num == 4:
+        return [[1,0,0,1,0], [0,1,0,1,0], [1,1,1,0,0]]
+    elif num == 5:
+        return [[1,1,0,1,0], [0,0,1,1,0], [0,0,0,0,1]]
+    elif num == 6:
+        return [[1,0,0,0,1], [0,1,0,0,1]]
+    elif num == 7:
+        return [[1,1,0,0,1], [1,1,1,1,0],[0,0,1,0,1]]
+    elif num == 8:
+        return [[0,0,0,1,1], [0,1,1,0,1], [1,0,1,0,1]]
+    elif num == 9:
+        return [[1,1,1,0,1], [0,1,0,1,1], [1,0,0,1,1]]
+    elif num == 10:
+        return [[0,0,1,1,1]]
+    elif num == 11:
+        return [[1,0,1,1,1], [0,1,1,1,1]]
 
-    return flags
-
-def main(argv):
-    if not is_valid(sys.argv):
-      print("rrrrr")
-      return
-
-    hour   = int(sys.argv[1])
-    minute = int(sys.argv[2])
-
-    minute = int(minute/5)
-    
+def main(hour, minute):
     hour_flags   = calc_flags(hour)
     minute_flags = calc_flags(minute)
     
-    ans = []
-    for h, m in zip(hour_flags, minute_flags):
-        if h == 1 and  m == 1:
-            ans.append("g")
-        elif h == 0 and m == 1:
-            ans.append("r")
-        elif h == 1 and m == 0:
-            ans.append("b")
+    for hour_flag in hour_flags:
+        for minute_flag in minute_flags:
+            answer = ""
+            for i,j in zip(hour_flag, minute_flag):
+                if i == 1 and j == 0:
+                    answer += "b"
+                elif i == 0 and j == 1:
+                    answer += "r"
+                elif i == 1 and j == 1:
+                    answer += "g"
+                else:
+                    break
 
-    for e in ans:
-        print(e, end="")
+            if len(answer) == 5:
+                return answer
+    return "rrrrr"
 
 if __name__=="__main__":
-    main(sys.argv)
+    if is_valid(sys.argv):
+        hour   = int(sys.argv[1])
+        minute = int(sys.argv[2])
+        answer = main(hour, int(minute / 5))
+        print(answer)
+    else:
+        print("rrrrr")
